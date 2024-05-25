9e92e822b11f3512729a6f427492cf39ab09a36b
홍정민(GitMaster) <jm90.hong@gmail.com>
2024-05-25 21:31:52 +0900

    1

---
+++ src/main/java/com/my/techit_spring_backend/TechitSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.my.techit_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechitSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechitSpringBackendApplication.class, args);
+	}
+
+}
